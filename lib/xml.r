83936aebe029b36d69668b888145a98844400f2e
Spela Dugar <spela.dugar@gmail.com>
2014-11-26 12:06:18 +0100

    Uvoz tabele

--- lib/xml.r
+++ lib/xml.r
@@ -6,11 +6,11 @@ library(XML)
 # iz vozlišč, ki ustrezajo podani poti.
 stripByPath <- function(x, path) {
   unlist(xpathApply(x, path,
-                    function(y) gsub("^\\s*(.*?)\\s*$", "\\1", xmlValue(y))))
+                    function(y) gsub("^\\s*(.*?)Â?\\s*$", "\\1", xmlValue(y[[1]]))))
 }
 
 uvozi.obcine <- function() {
-  url.obcine <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
+  url.obcine <- "http://epp.eurostat.ec.europa.eu/tgm/table.do?tab=table&init=1&plugin=1&language=en&pcode=tps00111"
   doc.obcine <- htmlTreeParse(url.obcine, useInternalNodes=TRUE)
   
   # Poiščemo vse tabele v dokumentu
@@ -18,21 +18,22 @@ uvozi.obcine <- function() {
   
   # Iz druge tabele dobimo seznam vrstic (<tr>) neposredno pod
   # trenutnim vozliščem
-  vrstice <- getNodeSet(tabele[[2]], "./tr")
+  vrstice <- getNodeSet(tabele[[6]], ".//tr")
   
   # Seznam vrstic pretvorimo v seznam (znakovnih) vektorjev
   # s porezanimi vsebinami celic (<td>) neposredno pod trenutnim vozliščem
-  seznam <- lapply(vrstice[2:length(vrstice)], stripByPath, "./td")
+  seznam <- lapply(vrstice, stripByPath, "./td/div")
   
   # Iz seznama vrstic naredimo matriko
   matrika <- matrix(unlist(seznam), nrow=length(seznam), byrow=TRUE)
   
   # Imena stolpcev matrike dobimo iz celic (<th>) glave (prve vrstice) prve tabele
-  colnames(matrika) <- gsub("\n", " ", stripByPath(tabele[[2]][[1]], ".//th"))
+  colnames(matrika) <- gsub("\n", " ", stripByPath(tabele[[4]], ".//th/div"))
+  rownames(matrika) <- gsub("\n", " ", stripByPath(tabele[[5]], ".//th"))
   
   # Podatke iz matrike spravimo v razpredelnico
-  return(data.frame(apply(gsub("\\*", "",
-                          gsub(",", ".",
-                          gsub("\\.", "", matrika[,2:5]))),
-                    2, as.numeric), row.names=matrika[,1]))
+  return(
+    data.frame(apply(gsub("[,:]", "", matrika),
+                    2, as.numeric))
+    )
 }
