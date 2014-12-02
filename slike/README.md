b968d4dfde1adcf2c22bf038179ca48fbcd0804d
Spela Dugar <spela.dugar@gmail.com>
2014-12-02 14:40:32 +0100

    Naredila sem graf.

--- slike/README.md
+++ slike/README.md
@@ -2,3 +2,5 @@
 
 V to mapo bomo dajali vse slike (grafe, zemljevide), ki jih bo naš program
 naredil in jih bomo vključili v končno poročilo.
+
+barplot(RodnostSLO[158:169,3],xlab="imena regij", ylab="št. živorojenih otrok", main="Rodnost v SLO leta 2013 po regijah", col="blue")
--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,19 +1,37 @@
 # 2. faza: Uvoz podatkov
 
+# # Funkcija, ki uvozi podatke iz datoteke druzine.csv
+# uvoziDruzine <- function() {
+#   return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
+#                       row.names = 1,
+#                       col.names = c("obcina", "en", "dva", "tri", "stiri"),
+#                       fileEncoding = "Windows-1250"))
+# }
+# 
+# # Zapišimo podatke v razpredelnico druzine.
+# cat("Uvažam podatke o družinah...\n")
+# druzine <- uvoziDruzine()
+# 
+# # Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
+# # potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
+# # datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
+# # 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
+# # fazah.
+
+
 # Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvoziDruzine <- function() {
-  return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
-                      row.names = 1,
-                      col.names = c("obcina", "en", "dva", "tri", "stiri"),
-                      fileEncoding = "Windows-1250"))
+uvoziSLO <- function() {
+  r <- read.csv("podatki/RodnostSLO.csv", sep = ";", as.is = TRUE,
+                    skip = 1,
+                    
+                    fileEncoding = "Windows-1250")
+
+  r <- r[is.na(r[1]),]
+  r[1] <- as.vector(sapply(2002:2013, rep, 13))
+  rownames(r) <- NULL
+  return(r)
 }
 
 # Zapišimo podatke v razpredelnico druzine.
-cat("Uvažam podatke o družinah...\n")
-druzine <- uvoziDruzine()
-
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
+cat("Uvažam podatke o RodnostiSLO...\n")
+RodnostSLO <- uvoziSLO()
