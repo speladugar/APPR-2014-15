c666c06b5d74e9ece41eaae8626c91265f2bbd7d
Spela Dugar <spela.dugar@gmail.com>
2014-12-02 14:54:08 +0100

    Naredila sem graf.

--- slike/grafi.R
+++ slike/grafi.R
@@ -1,3 +1,5 @@
 pdf("slike/grafi.pdf", paper="a4r")
-  barplot(r2013$Vrstni.red.rojstva...SKUPAJ, names.arg=r2013$X.1, xlab="imena regij", ylab="št.živorojenih otrok", main="Rodnost v SLO po regijah za leto 2013")
+  barplot(r2013$Vrstni.red.rojstva...SKUPAJ, names.arg=r2013$X.1, xlab="imena regij", ylab="št.živorojenih otrok", main="Rodnost v SLO po regijah za leto 2013",col="lightblue")
 dev.off()
+
+
