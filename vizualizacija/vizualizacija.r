17af1ed9b27fdb0785b60ff167aae34f59badeb3
Spela Dugar <spela.dugar@gmail.com>
2014-12-17 11:28:20 +0100

    Naredila sem zemljevid za RodnostSLO.

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -5,20 +5,18 @@ source("lib/uvozi.zemljevid.r")
 
 # Uvozimo zemljevid.
 cat("Uvažam zemljevid...\n")
-obcine <- uvozi.zemljevid("http://e-prostor.gov.si/fileadmin/BREZPLACNI_POD/RPE/OB.zip",
-                          "obcine", "OB/OB.shp", mapa = "zemljevid",
-                          encoding = "Windows-1250")
-
+slo <- uvozi.zemljevid("http://biogeo.ucdavis.edu/data/gadm2/shp/SVN_adm.zip",
+                       "slovenija", "SVN_adm1.shp", mapa = "zemljevid",
+                       encoding = "Windows-1250")
 # Funkcija, ki podatke preuredi glede na vrstni red v zemljevidu
 preuredi <- function(podatki, zemljevid) {
-  nove.obcine <- c()
-  manjkajo <- ! nove.obcine %in% rownames(podatki)
+  nove.slo <- c()
+  manjkajo <- ! nove.slo %in% rownames(podatki)
   M <- as.data.frame(matrix(nrow=sum(manjkajo), ncol=length(podatki)))
   names(M) <- names(podatki)
-  row.names(M) <- nove.obcine[manjkajo]
+  row.names(M) <- nove.slo[manjkajo]
   podatki <- rbind(podatki, M)
-  
-  out <- data.frame(podatki[order(rownames(podatki)), ])[rank(levels(zemljevid$OB_UIME)[rank(zemljevid$OB_UIME)]), ]
+  out <- data.frame(podatki[order(rownames(podatki)), ])[rank(levels(zemljevid$NAME_1)[rank(zemljevid$NAME_1)]), ]
   if (ncol(podatki) == 1) {
     out <- data.frame(out)
     names(out) <- names(podatki)
@@ -28,19 +26,19 @@ preuredi <- function(podatki, zemljevid) {
 }
 
 # Preuredimo podatke, da jih bomo lahko izrisali na zemljevid.
-druzine <- preuredi(druzine, obcine)
+zRodnostSLO <- preuredi(RodnostSLO[145:156,], slo)
 
 # Izračunamo povprečno velikost družine.
-druzine$povprecje <- apply(druzine[1:4], 1, function(x) sum(x*(1:4))/sum(x))
-min.povprecje <- min(druzine$povprecje, na.rm=TRUE)
-max.povprecje <- max(druzine$povprecje, na.rm=TRUE)
+min.2013 <- min(zRodnostSLO[4], na.rm=TRUE)
+max.2013 <- max(zRodnostSLO[4], na.rm=TRUE)
+norm.2013 <- (zRodnostSLO[4]-min.2013)/(max.2013-min.2013)
 
 # Narišimo zemljevid v PDF.
 cat("Rišem zemljevid...\n")
 pdf("slike/povprecna_druzina.pdf", width=6, height=4)
 
 n = 100
-barve = topo.colors(n)[1+(n-1)*(druzine$povprecje-min.povprecje)/(max.povprecje-min.povprecje)]
-plot(obcine, col = barve)
+barve =topo.colors(n)[unlist(1+(n-1)*norm.2013)]
+plot(slo, col = barve)
 
 dev.off()
