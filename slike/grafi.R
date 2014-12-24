3a2e7e06e45a4fa517c9fd4eb045f25f5dc9275e
Spela Dugar <spela.dugar@gmail.com>
2014-12-24 10:54:15 +0100

    Popravila graf za Slovenijo.

--- slike/grafi.R
+++ slike/grafi.R
@@ -1,5 +1,8 @@
 pdf("slike/grafi.pdf", paper="a4r")
-  barplot(r2013$Vrstni.red.rojstva...SKUPAJ, names.arg=r2013$X.1, xlab="imena regij", ylab="št.živorojenih otrok", main="Rodnost v SLO po regijah za leto 2013",col="lightblue")
+  
+barplot(RodnostSLO[145:156, 3], names.arg=RodnostSLO[145:156, 2], xlab="imena regij", ylab="št.živorojenih otrok", main="Rodnost v SLO po regijah za leto 2013",col="lightblue")
+
 barplot(as.matrix(RodnostEU[1,1:11]), names.arg = 2002:2012, xlab="leta", ylab="št.živorojenih v EU(28)", main="Število živorojenih otrok v EU(28) po letih 2002-2012", col="pink")
+
 dev.off()
 
