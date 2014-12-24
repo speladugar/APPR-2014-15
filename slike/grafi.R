75152fb3ff919a959bcf3fb4f469b0c22fd47dae
Spela Dugar <spela.dugar@gmail.com>
2014-12-24 11:35:36 +0100

    Spremenila poročilo.

--- slike/grafi.R
+++ slike/grafi.R
@@ -1,6 +1,6 @@
 pdf("slike/grafi.pdf", paper="a4r")
   
-barplot(RodnostSLO[145:156, 3], names.arg=RodnostSLO[145:156, 2], xlab="imena regij", ylab="št.živorojenih otrok", main="Rodnost v SLO po regijah za leto 2013",col="lightblue")
+barplot(RodnostSLO[145:156, 3], names.arg=RodnostSLO[145:156, 2],las=2, ylab="št.živorojenih otrok", main="Rodnost v SLO po regijah za leto 2013",col="lightblue")
 
 barplot(as.matrix(RodnostEU[1,1:11]), names.arg = 2002:2012, xlab="leta", ylab="št.živorojenih v EU(28)", main="Število živorojenih otrok v EU(28) po letih 2002-2012", col="pink")
 
